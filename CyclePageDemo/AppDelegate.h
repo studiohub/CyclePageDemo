9a915c385e795c20107859dd51b0cc1a9fa338c8
goyaya <goyaya@yeah.net>
2016-09-01 16:55:12 +0800

    Initial Commit

---
+++ CyclePageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CyclePageDemo
+//
+//  Created by 高洋 on 16/9/1.
+//  Copyright © 2016年 高洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CyclePageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyclePageDemo
+//
+//  Created by 高洋 on 16/9/1.
+//  Copyright © 2016年 高洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
